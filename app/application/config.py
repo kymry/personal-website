53d818bc4aaa268c3ac982195bb8d24ba7295e4a
kymry burwell <kymry.burwell@gmail.com>
2020-03-15 21:39:34 -0700

    Fixed sentiment analysis buttons and style

--- app/application/config.py
+++ app/application/config.py
@@ -7,4 +7,3 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or "6IxOGEa58UtIzWujwoTAIzzsb832OcLmr8V-mNSBoA4"
     #DATABASE = os.environ.get('DATABASE') or os.path.join(basedir, 'databases/astronomicaldata.db')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
--- app/wsgi.py
+++ app/wsgi.py
@@ -5,5 +5,5 @@ app = create_app()
 
 if __name__ == "__main__":
 	# Model is nearly 120mb. It must be loaded prior to launching the Flask app.
-	#model.preload_model()
+	model.preload_model()
 	app.run(host='0.0.0.0', threaded=False)
